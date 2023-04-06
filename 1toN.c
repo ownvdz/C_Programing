158f6f7d9030c732f259a208950d21eff1329b80
ownvdz <128668961+ownvdz@users.noreply.github.com>
2023-04-06 14:17:50 +0900

    Add files via upload

---
+++ 1toN.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void) {
+    int i=1, n, sum=0;
+    printf("정수를 입력하시오: ");
+    scanf("%d", &n);
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+    printf("1부터 %d까지의 합은 %d입니다", n, sum);
+}
+//2023-04-06
