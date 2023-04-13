f4c22e41140bb7393e08e4b4992b1eccdb40d529
ownvdz <128668961+ownvdz@users.noreply.github.com>
2023-04-13 14:13:12 +0900

    Add files via upload

---
+++ print_star.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void print_stars() {
+    for(int i=0; i<30; i++) {
+        printf("*");
+    }
+}
+
+int main(void) {
+    print_stars();
+    printf("\nHello World!\n");
+    print_stars();
+    printf("\n");
+    return 0;
+}
+//2023-04-13
