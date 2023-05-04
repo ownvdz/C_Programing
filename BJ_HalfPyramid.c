13b79080c61330cae485dc3bd606dedf10d1af4b
ownvdz <128668961+ownvdz@users.noreply.github.com>
2023-05-04 14:04:34 +0900

    Add files via upload

---
+++ BJ_HalfPyramid.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void) {
+    int num;
+    scanf("%d", &num);
+
+    for(int i=1; i<=num; i++) {
+        for(int j=num-i; j>0; j--) {
+            printf(" ");
+        }
+        for(int j=1; j<=i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
